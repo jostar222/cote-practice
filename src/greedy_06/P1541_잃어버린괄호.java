e7737373ebd89bd175c397b81b2e8a01cbae7716
Jostar222 <whtjdgus911@gmail.com>
2024-11-25 22:22:41 +0900

    06 그리디 06-1 그리디 알고리즘 - 잃어버린괄호

---
+++ src/greedy_06/P1541_잃어버린괄호.java
@@ -0,0 +1,30 @@
+package greedy_06;
+
+import java.util.Scanner;
+
+public class P1541_잃어버린괄호 {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String example = sc.nextLine();
+        String[] str = example.split("-");
+        for (int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if (i == 0) {
+                answer = answer + temp; // 가장 앞에 있는 값만 더함
+            } else {
+                answer = answer - temp;
+            }
+        }
+        System.out.println(answer);
+    }
+    static int mySum(String a) { // 나뉜 그룹의 더하기 연산 수행 함수
+        int sum = 0;
+        String temp[] = a.split("[+]");
+        for (int i = 0; i < temp.length; i++) {
+            sum += Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
+
