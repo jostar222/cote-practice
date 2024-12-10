59a491cf47643894bce14e5fed4509329e43a0a1
Jostar222 <whtjdgus911@gmail.com>
2024-12-10 22:11:33 +0900

    07 정수론 07-3 유클리드 호제법 - 최대공약수

---
+++ src/numberTheory_07/P1850_최대공약수.java
@@ -0,0 +1,25 @@
+package numberTheory_07;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class P1850_최대공약수 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long result = gcd(a,b);
+        while (result > 0) {
+            bw.write("1");
+            result--;
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static long gcd(long a, long b) {
+        if(b == 0) return a;
+        else return gcd(b, a % b);
+    }
+}
